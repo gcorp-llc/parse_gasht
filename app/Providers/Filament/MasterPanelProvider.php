8b9b74d57f4b0691d9286b32904e18be4b59cfaa
GCORP LLC <hosseinradvictor@gmail.com>
2024-10-30 10:39:05 +0330

    Make Admin Panel Run Queue Wedget

--- app/Providers/Filament/MasterPanelProvider.php
+++ app/Providers/Filament/MasterPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Filament\Resources\NResource\Widgets\RunCountryJobWidget;
 
 class MasterPanelProvider extends PanelProvider
 {
@@ -37,8 +38,10 @@ class MasterPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
+                RunCountryJobWidget::class,
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+
             ])
             ->middleware([
                 EncryptCookies::class,
